5547e9ba4e18ba0c9953ab4a56df30ddf1edca4f
sudoflux <jfletcherj86@gmail.com>
2025-04-19 01:59:45 -0400

    Corrected lua issue on theme and font

--- .config/nvim/lua/config/plugins/font.lua
+++ .config/nvim/lua/config/plugins/font.lua
@@ -1,7 +1,8 @@
--- Font-specific UI tweaks (if using GUI like Neovide, etc.)
-if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font:h12"
-end
-
--- Enable devicons (for file tree, status line, etc.)
-require("nvim-web-devicons").setup({})
+return {
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup({})
+    end,
+  },
+}
--- .config/nvim/lua/config/plugins/themes.lua
+++ .config/nvim/lua/config/plugins/themes.lua
@@ -1,10 +1,22 @@
--- Basic colorscheme setup
-vim.opt.termguicolors = true
-vim.cmd.colorscheme("tokyonight") -- Or any other colorscheme you've added via Lazy
+return {
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme("tokyonight")
 
--- Optional: transparency support
-vim.cmd([[
-  highlight Normal guibg=NONE ctermbg=NONE
-  highlight NormalNC guibg=NONE ctermbg=NONE
-  highlight SignColumn guibg=NONE ctermbg=NONE
-]])
+      -- Optional transparency
+      vim.cmd([[
+        highlight Normal guibg=NONE ctermbg=NONE
+        highlight NormalNC guibg=NONE ctermbg=NONE
+        highlight SignColumn guibg=NONE ctermbg=NONE
+      ]])
+
+      -- GUI font setting for Neovide, etc.
+      if vim.g.neovide then
+        vim.o.guifont = "JetBrainsMono Nerd Font:h12"
+      end
+    end,
+  },
+}
